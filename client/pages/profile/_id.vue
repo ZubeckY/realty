4d8c608146338f1737b01b5ddb8140bc309476e3
oleg <oleg_zub00@mail.ru>
2024-04-03 15:28:40 +0300

    Пользователь не найден

--- client/pages/profile/_id.vue
+++ client/pages/profile/_id.vue
@@ -1,27 +1,35 @@
 <template>
   <section class="profile">
-    <div class="profile-container">
+    <div v-if="!loading" class="profile-container">
       <div class="profileInfo">
         <div class="profileInfo-container">
           <card class="profilePhoto">
             <div class="profilePhoto-container">
               <div class="profilePhoto__wrapper">
                 <img
-                  class="profilePhoto__img"
-                  alt="profilePhoto-avatar"
-                  :src="profilePhoto"
+                    class="profilePhoto__img"
+                    alt="profilePhoto-avatar"
+                    :src="profilePhoto"
                 />
+
+                <div v-if="!user" class="profilePhoto__unknown d-flex justify-center align-center">
+                  <v-icon size="90">mdi-help</v-icon>
+                </div>
+              </div>
+
+              <div v-if="!user">
+                <h4 class="profileCard-title text-center mt-2 mb-1">Пользователь не найден!</h4>
               </div>
 
               <div class="mt-2" v-if="currentUserItsMe">
                 <v-btn
-                  elevation="0"
-                  class="radius-small"
-                  :color="usableColor"
-                  @click="editMode = !editMode"
-                  outlined
-                  block
-                  small
+                    elevation="0"
+                    class="radius-small"
+                    :color="usableColor"
+                    @click="editMode = !editMode"
+                    outlined
+                    block
+                    small
                 >
                   Редактировать
                 </v-btn>
@@ -31,51 +39,51 @@
 
               <div class="mt-2" v-if="currentUserItsMe">
                 <v-btn
-                  elevation="0"
-                  class="radius-small"
-                  :color="usableColor"
-                  @click="devicesList = !devicesList"
-                  outlined
-                  block
-                  small
+                    elevation="0"
+                    class="radius-small"
+                    :color="usableColor"
+                    @click="devicesList = !devicesList"
+                    outlined
+                    block
+                    small
                 >
-                  Устройства
+                  Устройства входа
                 </v-btn>
 
                 <action-dialog
-                  v-model="devicesList"
-                  title="Активные устройства входа"
-                  text=""
-                  :popup="true"
+                    v-model="devicesList"
+                    title="Активные устройства входа"
+                    text=""
+                    :popup="true"
                 >
                   <div class="profileCard-devicesList">
                     <v-list
-                      v-for="ud in deviceList"
-                      :key="'user-device-item-' + ud.id"
-                      class="profileCard-devicesList__container"
-                      color="transparent"
-                      :dark="usableTheme"
-                      dense
+                        v-for="ud in deviceList"
+                        :key="'user-device-item-' + ud.id"
+                        class="profileCard-devicesList__container"
+                        color="transparent"
+                        :dark="usableTheme"
+                        dense
                     >
                       <v-list-item
-                        three-line
-                        class="profileCard-devicesList__item pa-1"
+                          three-line
+                          class="profileCard-devicesList__item pa-1"
                       >
                         <v-list-item-content>
                           <v-list-item-title
-                            class="d-flex flex-row justify-space-between"
+                              class="d-flex flex-row justify-space-between"
                           >
-                            <div>{{ ud['device'] }}</div>
-                            <div>{{ normalizeLastSeen(ud['lastSeen']) }}</div>
+                            <div>{{ ud["device"] }}</div>
+                            <div>{{ normalizeLastSeen(ud["lastSeen"]) }}</div>
                           </v-list-item-title>
                           <v-list-item-subtitle
-                            class="d-flex flex-row justify-space-between align-center py-1"
+                              class="d-flex flex-row justify-space-between align-center py-1"
                           >
-                            <div>{{ ud['userAgent'] }}</div>
+                            <div>{{ ud["userAgent"] }}</div>
                             <div>
                               <div
-                                v-if="isCurrentAgent(ud['value'])"
-                                class="success darken-1 pa-1 radius-small"
+                                  v-if="isCurrentAgent(ud['value'])"
+                                  class="success white--text darken-1 pa-1 radius-small"
                               >
                                 Текущий
                               </div>
@@ -85,7 +93,7 @@
                             </div>
                           </v-list-item-subtitle>
                           <v-list-item-subtitle>
-                            {{ ud['ip'] }}
+                            {{ ud["ip"] }}
                           </v-list-item-subtitle>
                         </v-list-item-content>
                       </v-list-item>
@@ -96,29 +104,29 @@
 
               <div class="mt-2" v-if="currentUserItsMe">
                 <v-btn
-                  elevation="0"
-                  class="radius-small"
-                  color="error darken-1"
-                  @click="exitDialog = !exitDialog"
-                  outlined
-                  block
-                  small
+                    elevation="0"
+                    class="radius-small"
+                    color="error darken-1"
+                    @click="exitDialog = !exitDialog"
+                    outlined
+                    block
+                    small
                 >
                   Выйти из профиля
                 </v-btn>
 
                 <action-dialog
-                  v-model="exitDialog"
-                  title="Выйти из профиля?"
-                  text="Вы действительно хотите выйти из профиля?"
-                  @isConfirm="exitFromProfile"
+                    v-model="exitDialog"
+                    title="Выйти из профиля?"
+                    text="Вы действительно хотите выйти из профиля?"
+                    @isConfirm="exitFromProfile"
                 />
               </div>
             </div>
           </card>
 
           <section class="profileSection ml-2">
-            <div>
+            <div v-if="user">
               <card class="profileCard mb-2">
                 <div class="profileCard-container">
                   <h4 class="profileCard-title mb-2">Линчая информация</h4>
@@ -126,21 +134,21 @@
                   <div class="profileInfo-infoGroup mr-2">
                     <div class="profileInfo-infoGroup__title">Имя</div>
                     <div class="profileInfo-infoGroup__value">
-                      {{ user.firstName }}
+                      {{ user.firstName ?? "" }}
                     </div>
                   </div>
 
                   <div class="profileInfo-infoGroup">
                     <div class="profileInfo-infoGroup__title">Фамилия</div>
                     <div class="profileInfo-infoGroup__value">
-                      {{ user.lastName }}
+                      {{ user.lastName ?? "" }}
                     </div>
                   </div>
 
                   <div class="profileInfo-infoGroup">
                     <div class="profileInfo-infoGroup__title">Отчество</div>
                     <div class="profileInfo-infoGroup__value">
-                      {{ user.patronymic ?? 'не указано' }}
+                      {{ user.patronymic ?? "не указано" }}
                     </div>
                   </div>
 
@@ -149,14 +157,14 @@
                       Дата рождения
                     </div>
                     <div class="profileInfo-infoGroup__value">
-                      {{ user.dateBirthday ?? 'не указано' }}
+                      {{ user.dateBirthday ?? "не указано" }}
                     </div>
                   </div>
 
                   <div class="profileInfo-infoGroup mr-2">
                     <div class="profileInfo-infoGroup__title">О себе</div>
                     <div class="profileInfo-infoGroup__value">
-                      {{ user.aboutMe }}
+                      {{ user.aboutMe ?? "" }}
                     </div>
                   </div>
                 </div>
@@ -170,8 +178,8 @@
                     <div class="profileInfo-infoGroup__title">Телефон</div>
                     <div class="profileInfo-infoGroup__value">
                       <a v-if="user.phone" :href="'tel:' + user.phone">{{
-                        user.phone ?? 'не указано'
-                      }}</a>
+                          user.phone ?? "не указано"
+                        }}</a>
                       <span v-else>не указано</span>
                     </div>
                   </div>
@@ -194,28 +202,28 @@
                   <div class="profileInfo-infoGroup">
                     <div class="profileInfo-infoGroup__title">Роль</div>
                     <div class="profileInfo-infoGroup__value">
-                      {{ user.role }}
+                      {{ user.role ?? "" }}
                     </div>
                   </div>
 
                   <div class="profileInfo-infoGroup">
                     <div class="profileInfo-infoGroup__title">Агентство</div>
                     <div class="profileInfo-infoGroup__value">
-                      {{ userAgency }}
+                      {{ userAgency ?? "" }}
                     </div>
                   </div>
 
                   <div class="profileInfo-infoGroup">
                     <div class="profileInfo-infoGroup__title">Должность</div>
                     <div class="profileInfo-infoGroup__value">
-                      {{ user.jobTitle ?? 'не указано' }}
+                      {{ user.jobTitle ?? "не указано" }}
                     </div>
                   </div>
                 </div>
               </card>
             </div>
 
-            <div>
+            <div v-if="user">
               <card class="profileCard ml-2 mb-2">
                 <div class="profileCard-container">
                   <h4 class="profileCard-title mb-2">Эффективность</h4>
@@ -225,7 +233,7 @@
                       Кол-во продаж
                     </div>
                     <div class="profileInfo-infoGroup__value">
-                      {{ user.numberOfSales }}
+                      {{ user.numberOfSales ?? "" }}
                     </div>
                   </div>
 
@@ -234,42 +242,60 @@
                       Тариф менеджера
                     </div>
                     <div class="profileInfo-infoGroup__value">
-                      {{ user.coefficient }}
+                      {{ user.coefficient ?? "" }}
                     </div>
                   </div>
 
                   <div class="profileInfo-infoGroup">
                     <div class="profileInfo-infoGroup__title">Сумма продаж</div>
                     <div class="profileInfo-infoGroup__value">
-                      {{ user.amountSales }}
+                      {{ user.amountSales ?? "" }}
                     </div>
                   </div>
                 </div>
               </card>
             </div>
           </section>
+
+          <v-snackbar
+              v-model="snackbar"
+              :color="snackbarColor"
+              :timeout="2000"
+              outlined
+              text
+          >
+            {{ snackbarMessage }}
+          </v-snackbar>
+
         </div>
       </div>
     </div>
   </section>
 </template>
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import { ColorTheme } from '~/assets/script/functions/colorTheme'
+import { Component, Vue } from "vue-property-decorator"
+import { ColorTheme } from "~/assets/script/functions/colorTheme"
+import axiosAuthConfig from "~/assets/script/functions/axiosAuthConfig"
 
 @Component
 export default class Profile extends Vue {
+  user: any = {}
+
+  loading: boolean = true
+
   editMode: boolean = false
   exitDialog: boolean = false
+
+  snackbar: boolean = false
+  snackbarColor: string = ""
+  snackbarMessage: string = ""
 
   deviceList: any = []
   devicesList: boolean = false
 
-  user: any = {}
-
   async created() {
     let currentRoute = this.$router.currentRoute.path
-    let array = currentRoute.split('/')
+    let array = currentRoute.split("/")
     let id = array[array.length - 1]
 
     if (!Number(id)) return
@@ -279,21 +305,21 @@ export default class Profile extends Vue {
 
   async getAuthUserDevices() {
     await this.$axios
-      .post('/api/auth/get-my-devices/', {
-        id: this.user.id,
-      })
-      .then((data) => {
-        return (this.deviceList = data.data)
-      })
+        .post("/api/auth/get-my-devices/", {
+          id: this.user.id
+        })
+        .then((data) => {
+          return (this.deviceList = data.data)
+        })
   }
 
   get checkIDToValid() {
-    const route: string[] = this.$router.currentRoute.path.split('/')
+    const route: string[] = this.$router.currentRoute.path.split("/")
     const numericNeedID = route[route.length - 1]
 
     // Проверяем на цифру
     if (!Number.isInteger(Number(numericNeedID))) {
-      return console.log('Неверное значение id')
+      return console.log("Неверное значение id")
     }
 
     return +numericNeedID
@@ -303,10 +329,39 @@ export default class Profile extends Vue {
     if (this.currentUserItsMe) {
       this.user = this.savedUser
       await this.getAuthUserDevices()
+
+      this.loading = false
+
       return
     }
 
-    return null
+    if (process.client) {
+      let authToken = localStorage.getItem("token")
+
+      if (!authToken) {
+        return null
+      }
+
+      await this.$axios
+          .get(
+              "/api/user/" + this.checkIDToValid,
+              {
+                ...axiosAuthConfig(authToken, "", "crm_client")
+              }
+          )
+          .then((data: any) => {
+            if (data.data?.message) {
+              this.setSnackbarValues("error darken-1", data.data.message)
+              console.log(data.data.error)
+              return
+            }
+
+            return this.user = data.data.user
+          })
+          .finally(() => {
+            this.loading = false
+          })
+    }
   }
 
   get savedUser() {
@@ -323,56 +378,56 @@ export default class Profile extends Vue {
 
   get profilePhoto() {
     return (
-      this.user.avatar ||
-      'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png'
+        this.user?.avatar?.src ||
+        "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
     )
   }
 
   normalizeLastSeen(date: any) {
     const DATE = new Date(date)
     const monthList = [
-      'янв',
-      'фев',
-      'мар',
-      'апр',
-      'май',
-      'июн',
-      'июл',
-      'авг',
-      'сен',
-      'окт',
-      'ноя',
-      'дек',
+      "янв",
+      "фев",
+      "мар",
+      "апр",
+      "май",
+      "июн",
+      "июл",
+      "авг",
+      "сен",
+      "окт",
+      "ноя",
+      "дек"
     ]
 
     return (
-      DATE.getDate() +
-      ' ' +
-      monthList[DATE.getMonth()] +
-      ' ' +
-      DATE.getFullYear()
+        DATE.getDate() +
+        " " +
+        monthList[DATE.getMonth()] +
+        " " +
+        DATE.getFullYear()
     )
   }
 
   isCurrentAgent(hash: string) {
     if (process.client) {
-      const token: any = localStorage.getItem('token')
+      const token: any = localStorage.getItem("token")
       return token.includes(hash)
     }
   }
 
   async exitFromProfile() {
     if (process.client) {
-      const token: any = localStorage.getItem('token')
+      const token: any = localStorage.getItem("token")
       await this.$axios
-        .post('/api/auth/logout/' + token)
-        .then((data) => {
-          localStorage.removeItem('token')
-          window.location.reload()
-        })
-        .catch((e) => {
-          console.log(e)
-        })
+          .post("/api/auth/logout/" + token)
+          .then((data) => {
+            localStorage.removeItem("token")
+            window.location.reload()
+          })
+          .catch((e) => {
+            console.log(e)
+          })
     }
   }
 
@@ -380,8 +435,14 @@ export default class Profile extends Vue {
     if (this.user.agency) {
       return this.user.agency.title
     } else {
-      return 'не указано'
+      return "не указано"
     }
+  }
+
+  setSnackbarValues(color: string, message: string) {
+    this.snackbar = true
+    this.snackbarColor = color
+    this.snackbarMessage = message
   }
 
   get usableTheme() {
--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,13 +1,11 @@
 import 'reflect-metadata'
-import { Param, Body, Get, Post, Put, Delete, OnUndefined, UseAfter, JsonController } from 'routing-controllers'
+import { Param, Body, Get, Post, Put, Delete, UseAfter, JsonController } from 'routing-controllers'
 import { checkAuth } from '../middleware/checkAuth.js'
-import { checkAgency } from '../middleware/checkAgency.js'
-import { checkPaymentToken } from '../middleware/checkPaymentToken.js'
+import { AppDataSource } from "../connectDataBase"
+import { User } from "../entity"
 
 @UseAfter(checkAuth)
-@UseAfter(checkAgency)
-@UseAfter(checkPaymentToken)
-@JsonController('/users')
+@JsonController('/user')
 export class UserController {
   @Get('/')
   getAll() {
@@ -16,10 +14,24 @@ export class UserController {
   }
 
   @Get('/:id')
-  @OnUndefined(204)
-  getOne(@Param('id') id: number) {
+  async getOne(@Param('id') id: number) {
     try {
-    } catch (e) {}
+      const userRepository = AppDataSource.getRepository(User)
+      const userFromDB = await userRepository
+        .createQueryBuilder('user')
+        .leftJoinAndSelect('user.photo', 'photo')
+        .leftJoinAndSelect('user.address', 'address')
+        .leftJoinAndSelect('user.agency', 'agency')
+        .where('user.id = :userId', { userId: id })
+        .getOne();
+
+      return userFromDB ? userFromDB : null
+    } catch (e) {
+      return {
+        message: 'Ошибка сервера, чтобы посмотреть подробнее, зайдите в консоль',
+        error: e,
+      }
+    }
   }
 
   @Post('/')

599695aec33ea2262e2d4cee6d14c65306ed39af
oleg <oleg_zub00@mail.ru>
2024-04-09 21:36:59 +0300

    oops

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -2,7 +2,7 @@ import { AdminController } from './adminController.js'
 import { AgencyController } from './agencyController.js'
 import { AuthController } from './authController.js'
 import { ClientController } from './clientController.js'
-import { FileController } from './fileController.js'
+// import { FileController } from './fileController.js'
 import { LeadController } from './leadController.js'
 import { NewsController } from './newsController.js'
 import { RealtyController } from './realtyController.js'
@@ -14,7 +14,7 @@ controllers.push(AdminController)
 controllers.push(AgencyController)
 controllers.push(AuthController)
 controllers.push(ClientController)
-controllers.push(FileController)
+// controllers.push(FileController)
 controllers.push(LeadController)
 controllers.push(NewsController)
 controllers.push(RealtyController)
